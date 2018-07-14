b9d0ab4736fff07b498edf4737c3094c2ae7f393
Mohammad Hossein Karami <itarfand@gmail.com>
2018-07-14 13:32:17 +0430

    add other file

---
+++ PHPChat/PHPWebProject1/PHPWebProject1/assets/js/file_label.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(document).on("change",
+        "#file",
+        function (parameters) {
+            var imageName = $("#file").val();
+            $("#file_label").html(imageName);
+        });
+});
